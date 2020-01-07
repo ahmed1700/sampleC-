fb47439e97c7ea467ccdbfaceb6b1edf8b38242d
admin <admin@LAB4-06>
2020-01-07 20:04:20 +0200

    test

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace System.ITShare
+{
+    public class MathOp
+    {
+        /// <summary>
+        /// sssssssssssssssssssssssssssssssss
+        /// </summary>
+        public void DoSomething()
+        {
+            Console.WriteLine("Do Something");
+        }
+        
+    }
+}
